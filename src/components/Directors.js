f4cfe9450f55591e06ed72aafcc3bd44b9c83969
jzolo22 <jzolotarev@gmail.com>
2020-12-19 18:45:47 -0500

    commit tree e2104b40e5f0bb1bd9ca0943e36e2723c7cdc322

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  function directorMovies(movies) {
+    return movies.map(movie => <li>{movie}</li>)
+  }
+
+  function allDirectors() {
+    return directors.map(director => {
+      return (
+      <div>
+        <h3>{director.name}</h3>
+        <ul>
+          {directorMovies(director.movies)}
+        </ul>
+      </div>)
+    })
+  }
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {allDirectors()}
     </div>
   );
 }

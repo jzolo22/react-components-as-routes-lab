cb1aecacc286a6ccba1966f0a20f427137f4fad0
jzolo22 <jzolotarev@gmail.com>
2020-12-19 18:45:53 -0500

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  function actorMovies(movies) {
+    return movies.map(movie => <li>{movie}</li>)
+  }
+
+  function allActors() {
+    return actors.map(actor => {
+      return (
+        <div>
+          <h3>{actor.name}</h3>
+          <ul>
+            {actorMovies(actor.movies)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {allActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  function movieGenres(genres) {
+    return genres.map(genre => {
+    return <li>{genre}</li>
+    })
+  }
+
+  function allMovies() {
+    return movies.map(movie => {
+      return (<div> 
+      <h1>{movie.title}</h1> 
+      <p>{movie.time} minutes</p>
+      <ul>
+        {movieGenres(movie.genres)}
+      </ul>
+    </div>)
+  })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {allMovies()}
     </div>
   );
 };
